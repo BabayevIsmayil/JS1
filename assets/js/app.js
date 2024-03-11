c6a050a53a1e30332529e075f64087641fbc2032
BabayevIsmayil <bismayil2005@gmail.com>
2024-03-11 15:43:01 +0400

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,24 @@
-var name = "Ismayil"
-var surname = "Babayev"
-prompt("ad yaz");
-console.log(age = 18);
-// abfjbdsjfnsdf sdfsadf dsf sdfkjbdfd
+// var name = "Revan" //string
+// var age = 7; //number
+// var status = true; //boolean
+// var name = "Ismayil";
+// console.log(name);
+// name = prompt("Adin nedi ?");
+// console.log(name);
+// var a = 5;
+// a = 6;   
+// console.log(a);
+// ADINI SOYADINI YASINI SORUSSUN . CONSOLE.LOG SALAM FILANKESOV FILANKES . SEN 18 YASINA 0 IL
+
+// REQEM YAZİRAM NECE İL AY GUN OLDUGUN DESİN 
+
+ 
+
+var name = prompt("adin nedi ?"); //string
+var surname = prompt("soyadin nedi ?"); //string
+var age = +prompt("yasin necedi ?")
+console.log(surname,name,age);
+console.log(18 - age);
+var qalib = 18 - age; 
+console.log(surname + " " + name + " , 18 yasina ", + qalib + " il qalir");
+
